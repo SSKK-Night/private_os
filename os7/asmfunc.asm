d75fce0709d9b6c471510d6b3fd12bd9bc7dbc8d
SSKK-Night <zhoupinglibo252@gmail.com>
2025-06-13 05:30:50 +0900

    asm

---
+++ os7/asmfunc.asm
@@ -0,0 +1,11 @@
+global LoadDT  ;  void LoadIDT(uint16_t limit, uint64_t offset);
+LoadIDT:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10
+    mov [rsp], di  ; limit
+    mov [rsp + 2], rsi  ; offset
+    lidt [rsp]
+    mov rsp, rbp
+    pop rbp
+    ret
