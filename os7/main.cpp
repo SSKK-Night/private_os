7fa270376118b31d1c63d95f29f55853935d2740
SSKK-Night <zhoupinglibo252@gmail.com>
2025-06-14 07:05:10 +0900

    mainの追加

--- os7/main.cpp
+++ os7/main.cpp
@@ -15,3 +15,12 @@ const uint16_t cs = GetCS()
 SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0),
                 reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
 LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]))
+
+
+const uint8_t bsp_local_apic_id = *reinterpret_cast<const uint32_t*>(0xfee00020) >> 24;
+
+pci::ConfigureMSIFixedDestination(
+    *xhc_dev, bsp_local_apic_id,
+    pci::MSITriggerMode::kLevel, pci::MSIDeliveryMode::kFixed,
+    InterruptVector::kXHCI, 0);
+
